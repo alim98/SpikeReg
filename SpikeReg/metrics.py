ba6b0aacc738829b0ca48c15bbe74df706a9b195
ArianFiroozi <arianfiroozi3@gmail.com>
2025-08-18 01:31:13 +0330

    feat: init potential metrics

---
+++ SpikeReg/metrics.py
@@ -0,0 +1,64 @@
+class Metrics:
+    @staticmethod
+    def dice_score(pred, target):
+        pass
+
+    @staticmethod
+    def jaccard_index(pred, target):
+        pass
+
+    @staticmethod
+    def hausdorff_distance(pred, target, percentile=95):
+        pass
+
+    @staticmethod
+    def local_negative_cross_correlation(pred, target, window_size=5):
+        pass
+
+    @staticmethod
+    def squared_error(pred, target):
+        pass
+
+    @staticmethod
+    def signal_to_noise_ratio(pred, target):
+        pass
+
+    @staticmethod
+    def structural_similarity_index(pred, target):
+        pass
+
+    @staticmethod
+    def f1_score(pred, target):
+        pass
+
+    @staticmethod
+    def nmi(pred, target):
+        pass
+
+    @staticmethod
+    def mutual_information(pred, target):
+        pass
+
+    @staticmethod
+    def intersection_over_union(pred, target):
+        pass
+
+    @staticmethod
+    def tre(pred, target):
+        pass
+
+    @staticmethod
+    def absolute_error(pred, target):
+        pass
+
+    @staticmethod
+    def surface_distance(pred, target):
+        pass
+
+    @staticmethod
+    def negative_jacobian(pred, target):
+        pass
+
+    @staticmethod
+    def std_jacobian(pred, target):
+        pass
